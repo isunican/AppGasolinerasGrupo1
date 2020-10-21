62fbfc71718998dd9bc97cb30e3eedb34eff848d
Adrian <acf142@alumnos.unican.es>
2020-10-22 00:52:33 +0200

    WIP: Realizados cambios en los tests de la DetailActivity
    Error actual: Class not found: "com.isunican.proyectobase.VistaDetalladaTest"

--- AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/VistaDetalladaTest.java
+++ AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/VistaDetalladaTest.java
@@ -2,15 +2,16 @@ package com.isunican.proyectobase;
 
 
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
 import com.isunican.proyectobase.Model.Gasolinera;
-import com.isunican.proyectobase.Presenter.PresenterGasolineras;
 import com.isunican.proyectobase.Views.MainActivity;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
@@ -23,21 +24,27 @@ import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 
+@RunWith(AndroidJUnit4.class)
 public class VistaDetalladaTest {
+
+    private Gasolinera g;
 
     @Rule
     public ActivityTestRule<MainActivity> activityRule =
             new ActivityTestRule<>(MainActivity.class);
 
+    @Before
+    public void init(){
+        g = new Gasolinera(0,"", "CANTABRIA", "CR N-629 79,7",
+                1.129, 1.215, "REPSOL");
+    }
+
     @Test
     public void vistaDetalladaGasolinera(){
-        Gasolinera g = new Gasolinera(0,"", "CANTABRIA", "CR N-629 79,7",
-                1.129, 1.215, "REPSOL");
-
         onData(allOf(is(instanceOf(Gasolinera.class)), is(g))).perform(click());
-        //onView(withId(R.id.nombreGasolineraText)).check(matches(withText("REPSOL")));
-        //onView(withId(R.id.direccionText)).check(matches(withText("Direccion:\nCR N-629 79,7")));
-        //onView(withId(R.id.precioGasoleoAText)).check(matches(withText("Gasoleo A: 1.129€")));
-        //onView(withId(R.id.precioGasoleo95Text)).check(matches(withText("Gasoleo 95: 1.215€")));
+        onView(withId(R.id.nombreGasolineraText)).check(matches(withText("REPSOL")));
+        onView(withId(R.id.direccionText)).check(matches(withText("Direccion:\nCR N-629 79,7")));
+        onView(withId(R.id.precioGasoleoAText)).check(matches(withText("Gasoleo A: 1.129€")));
+        onView(withId(R.id.precioGasoleo95Text)).check(matches(withText("Gasoleo 95: 1.215€")));
     }
 }
