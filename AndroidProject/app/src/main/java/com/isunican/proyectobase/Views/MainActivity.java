075d4d294a3a084e83c014622ab5475af50929a1
Elena <erl430@alumnos.unican.es>
2020-10-30 16:29:46 +0100

    WIP: Añadido botón en el panel deslizable
    Se ha añadido un botón en el panel deslizable del main activity para ir a la activity de añadir tarjeta de descuento

--- AndroidProject/app/src/main/java/com/isunican/proyectobase/Views/MainActivity.java
+++ AndroidProject/app/src/main/java/com/isunican/proyectobase/Views/MainActivity.java
@@ -179,6 +179,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 FiltrosActivity filtro = new FiltrosActivity();
                 filtro.show(getSupportFragmentManager(), "Dialog");
                 break;
+
+
+            case R.id.itemNuevaTarjetaDescuento:
+                Intent myIntent = new Intent(MainActivity.this, NuevaTarjetaDescuentoActivity.class);
+                MainActivity.this.startActivityForResult(myIntent, REQUEST_CODE_NEW_DISCOUNT_CARD);
+                break;
+
             default:
                 Log.d("MIGUEL", "Default en switch");
         }
@@ -401,5 +408,4 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             return view;
         }
     }
-
 }
