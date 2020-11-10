047423e31a47f9b9284b5b9475c6d74af54036b4
Adrian <acf142@alumnos.unican.es>
2020-11-11 00:01:55 +0100

    Aplicado correctamente patron Singleton

--- AndroidProject/app/src/main/java/com/isunican/proyectobase/Database/AppDatabase.java
+++ AndroidProject/app/src/main/java/com/isunican/proyectobase/Database/AppDatabase.java
@@ -1,6 +1,9 @@
 package com.isunican.proyectobase.Database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.isunican.proyectobase.DAO.GasolineraDAO;
@@ -11,7 +14,20 @@ import com.isunican.proyectobase.Model.GasolineraFavorita;
 @Database(entities = {Gasolinera.class, GasolineraFavorita.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     private static AppDatabase INSTANCE;
+
     public abstract GasolineraDAO gasolineraDAO();
+
     public abstract GasolineraFavoritaDAO gasolineraFavoritaDAO();
 
+    public static AppDatabase getInstance(Context context){
+        if(INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                               AppDatabase.class, "RoomDatabase").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
