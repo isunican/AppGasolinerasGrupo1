9f19b0991e37cfd1a87ffe606c22c81d18a420f6
Elena <erl430@alumnos.unican.es>
2020-11-11 09:44:19 +0100

    WIP: esqueleto de la lógica de eliminar favorito
    Se ha empezado el esqueleto de la lógica de eliminar filtro, a la espera de añadir comprobaciones, actualizar la base de datos, y mostrar mensajes en los casos de éxito y de error.

--- AndroidProject/app/src/main/java/com/isunican/proyectobase/Presenter/PresenterGasolinerasFavoritas.java
+++ AndroidProject/app/src/main/java/com/isunican/proyectobase/Presenter/PresenterGasolinerasFavoritas.java
@@ -2,11 +2,32 @@ package com.isunican.proyectobase.Presenter;
 
 import com.isunican.proyectobase.Model.Gasolinera;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PresenterGasolinerasFavoritas {
 
-    public Gasolinera eliminaGasolineraFavorita(Gasolinera gasolinera){
+    List<Gasolinera> gasolinerasFavoritas;
 
-        // hago cosas
-        return gasolinera;
+    public PresenterGasolinerasFavoritas(){
+        this.gasolinerasFavoritas = new ArrayList<>();
+    }
+
+    public Gasolinera eliminaGasolineraFavorita(int idGasolinera){
+
+        Gasolinera gasolineraEliminar = null;
+
+        for (int i = 0; i<gasolinerasFavoritas.size(); i++){
+            if (gasolinerasFavoritas.get(i).getIdeess() == idGasolinera){
+                gasolineraEliminar = gasolinerasFavoritas.get(i);
+                gasolinerasFavoritas.remove(i);
+            }
+        }
+
+        // TODO: Quitar gasolinera de la lista de la BD
+
+        // TODO: Añadir mensaje toast en caso de éxito y de fallo
+        
+        return gasolineraEliminar;
     }
 }
