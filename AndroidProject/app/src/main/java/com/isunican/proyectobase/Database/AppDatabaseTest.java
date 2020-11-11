8a51ead737f951948015da3546d7069d32249356
Adrian <acf142@alumnos.unican.es>
2020-11-11 19:18:49 +0100

    Generacion base de datos de test

---
+++ AndroidProject/app/src/main/java/com/isunican/proyectobase/Database/AppDatabaseTest.java
@@ -0,0 +1,33 @@
+package com.isunican.proyectobase.Database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.isunican.proyectobase.DAO.GasolineraDAO;
+import com.isunican.proyectobase.DAO.GasolineraFavoritaDAO;
+import com.isunican.proyectobase.Model.Gasolinera;
+import com.isunican.proyectobase.Model.GasolineraFavorita;
+
+@Database(entities = {Gasolinera.class, GasolineraFavorita.class}, version = 1, exportSchema = false)
+public abstract class AppDatabaseTest extends RoomDatabase {
+    private static AppDatabase INSTANCE;
+
+    public abstract GasolineraDAO gasolineraDAO();
+
+    public abstract GasolineraFavoritaDAO gasolineraFavoritaDAO();
+
+    public static AppDatabase getInstance(Context context){
+        if(INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            AppDatabase.class, "TestDatabase").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
