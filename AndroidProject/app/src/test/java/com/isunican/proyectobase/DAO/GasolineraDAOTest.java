4612df415b89558e470b160e46901074adb6fc2f
Adrian <acf142@alumnos.unican.es>
2020-11-13 00:33:10 +0100

    Test GasolineraDAO

--- AndroidProject/app/src/test/java/com/isunican/proyectobase/DAO/GasolineraDAOTest.java
+++ AndroidProject/app/src/test/java/com/isunican/proyectobase/DAO/GasolineraDAOTest.java
@@ -15,7 +15,6 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(RobolectricTestRunner.class)
@@ -57,10 +56,18 @@ public class GasolineraDAOTest   {
 
         // Caso 2: Tabla contiene el elemento buscado
         Gasolinera sut = new Gasolinera(1, "Santander", "Cantabria", "Los Castros Nº1", 1.00, 1.00, "CEPSA");
-        db.gasolineraDAO().insertOne(sut);
-        lista = db.gasolineraDAO().findById(1);
+        int res = db.gasolineraDAO().getIdFromRowId(db.gasolineraDAO().insertOne(sut));
+        lista = db.gasolineraDAO().findById(res);
         Assert.assertTrue(lista.size() == 1);
-        Assert.assertTrue(lista.get(0).getIdeess() == 1);
+        Assert.assertTrue(lista.get(0).equals(sut));
+
+        //Caso3: Dos o mas elementos en Tabla
+        Gasolinera sut2 = new Gasolinera(2, "Torrelavega", "Cantabria", "La inmobiliaria", 1.50, 1.39, "CEPSA");
+        res = db.gasolineraDAO().getIdFromRowId(db.gasolineraDAO().insertOne(sut2));
+        System.out.println(res);
+        lista = db.gasolineraDAO().findById(res);
+        Assert.assertTrue(lista.size() == 1);
+        Assert.assertTrue(lista.get(0).equals(sut2));
     }
 
     @Test
@@ -68,10 +75,10 @@ public class GasolineraDAOTest   {
         // Caso 1: Objeto nulo
         try{
             db.gasolineraDAO().insertOne(null);
-            Assert.fail();
+            ;
         } catch (Exception e)
         {
-            Assert.assertTrue(true);
+            Assert.assertTrue(db.gasolineraDAO().getAll().size()==0);
         }
         // Caso 2: Objeto correcto
         Gasolinera sut = new Gasolinera(3, "Santander", "Cantabria", "Los Castros Nº1", 1.00, 1.00, "CEPSA");
@@ -83,11 +90,55 @@ public class GasolineraDAOTest   {
     }
 
     @Test
-    public void insertAllTest(){
-        //al insertAll se le pasan lasgasolineras como parametros, no una lista
-        // Caso 1: Lista vacia
-        // Caso 2: Null
-        // Caso 3: Lista con 1 elemento
-        // Caso 4: Lista con mas 2 o mas elementos
+    public void deleteTest(){
+        //Caso 1: Eliminar nulo
+        try{
+            db.gasolineraDAO().delete(null);
+            Assert.fail();
+        } catch (Exception e)
+        {
+            Assert.assertTrue(db.gasolineraDAO().getAll().size()==0);
+        }
+        //Caso 2: Eliminar objeto no existente
+        Gasolinera sut = new Gasolinera(1, "Santander", "Cantabria", "Los Castros Nº1", 1.00, 1.00, "CEPSA");
+        db.gasolineraDAO().delete(sut);
+        Assert.assertTrue(db.gasolineraDAO().getAll().size()==0);
+
+        //Caso 3: Eliminar objeto existente
+        long rowid = db.gasolineraDAO().insertOne(sut);
+        sut.setId(db.gasolineraDAO().getIdFromRowId(rowid));
+        db.gasolineraDAO().delete(sut);
+        Assert.assertTrue(db.gasolineraDAO().getAll().size()==0);
+    }
+
+    @Test
+    public void nukeTest(){
+        // Caso 1: Tabla vacia
+        db.gasolineraDAO().nuke();
+        Assert.assertTrue(db.gasolineraDAO().getAll().size() == 0);
+        //Caso 2: Tabla con 1 elemento
+        Gasolinera sut = new Gasolinera(1, "Santander", "Cantabria", "Los Castros Nº1", 1.00, 1.00, "CEPSA");
+        db.gasolineraDAO().insertOne(sut);
+        db.gasolineraDAO().nuke();
+        Assert.assertTrue(db.gasolineraDAO().getAll().size() == 0);
+        // Caso 3: Tabla con 2 o mas elementos
+        Gasolinera sut2 = new Gasolinera(2, "Torrelavega", "Cantabria", "La inmobiliaria", 1.50, 1.39, "CEPSA");
+        Gasolinera sut3 = new Gasolinera(3, "Reinosa", "Cantabria", "Sin nombre", 1.30, 1.35, "CEPSA");
+        db.gasolineraDAO().insertOne(sut);
+        db.gasolineraDAO().insertOne(sut2);
+        db.gasolineraDAO().insertOne(sut3);
+        db.gasolineraDAO().nuke();
+        Assert.assertTrue(db.gasolineraDAO().getAll().size() == 0);
+    }
+
+    @Test
+    public void getIdFromRowIdTest(){
+        // Caso 1: No existente (Puedo poner el ID que quiera debido a que la tabla se encuentra vacia)
+        int res = db.gasolineraDAO().getIdFromRowId(27);
+        Assert.assertTrue(res == 0);
+        // Caso 2: Correcto
+        Gasolinera sut = new Gasolinera(1, "Santander", "Cantabria", "Los Castros Nº1", 1.00, 1.00, "CEPSA");
+        long rest = db.gasolineraDAO().insertOne(sut);
+        Assert.assertTrue(rest == 1);
     }
 }
