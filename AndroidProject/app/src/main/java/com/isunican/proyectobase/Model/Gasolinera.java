9f684f78ae156a844f56fe5b6c7f9898bb9f6837
Adrian <acf142@alumnos.unican.es>
2020-11-12 02:30:37 +0100

    Añadido método equals e id para la BD

--- AndroidProject/app/src/main/java/com/isunican/proyectobase/Model/Gasolinera.java
+++ AndroidProject/app/src/main/java/com/isunican/proyectobase/Model/Gasolinera.java
@@ -7,6 +7,8 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.util.Objects;
+
 
 /*
 ------------------------------------------------------------------
@@ -17,7 +19,9 @@ import androidx.room.PrimaryKey;
 */
 @Entity(tableName = "gasolinera")
 public class Gasolinera implements Parcelable {
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    @ColumnInfo
     private int ideess;
     @ColumnInfo
     private String localidad;
@@ -45,6 +49,8 @@ public class Gasolinera implements Parcelable {
         this.gasolina95 = gasolina95;
         this.rotulo = rotulo;
     }
+    public int getId(){ return id;}
+    public void setId(int id) { this.id = id; }
 
     public int getIdeess() { return ideess; }
 
@@ -83,6 +89,7 @@ public class Gasolinera implements Parcelable {
                 getRotulo() + "\n"+
                 getDireccion() + "\n" +
                 getLocalidad() + "\n" +
+                getIdeess() +"\n" +
                 "Precio diesel: " + getGasoleoA() + " " + "\n" +
                 "Precio gasolina 95: " + getGasolina95() + " " + "\n\n";
 
@@ -157,5 +164,24 @@ public class Gasolinera implements Parcelable {
             tiposGasolina += "Gasolina95 ";
         }
         return tiposGasolina;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Gasolinera that = (Gasolinera) o;
+        return ideess == that.ideess &&
+                Double.compare(that.gasoleoA, gasoleoA) == 0 &&
+                Double.compare(that.gasolina95, gasolina95) == 0 &&
+                Objects.equals(localidad, that.localidad) &&
+                Objects.equals(provincia, that.provincia) &&
+                Objects.equals(direccion, that.direccion) &&
+                Objects.equals(rotulo, that.rotulo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ideess, localidad, provincia, direccion, gasoleoA, gasolina95, rotulo);
     }
 }
