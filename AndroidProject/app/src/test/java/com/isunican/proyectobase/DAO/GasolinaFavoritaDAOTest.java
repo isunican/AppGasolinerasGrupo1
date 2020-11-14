93c5da3649548ed556604da017fa987e66833307
Adrian <acf142@alumnos.unican.es>
2020-11-14 17:53:43 +0100

    Correcion en la comprobacion de resultados v2

--- AndroidProject/app/src/test/java/com/isunican/proyectobase/DAO/GasolinaFavoritaDAOTest.java
+++ AndroidProject/app/src/test/java/com/isunican/proyectobase/DAO/GasolinaFavoritaDAOTest.java
@@ -30,43 +30,43 @@ public class GasolinaFavoritaDAOTest {
     @Test
     public void getAllTest(){
         // Caso 1: Tabla sin registros
-        Assert.assertTrue(db.gasolineraFavoritaDAO().getAll().size() == 0);
+        Assert.assertEquals(0, db.gasolineraFavoritaDAO().getAll().size());
 
         // Caso 2: Tabla con 1 solo resgistro
         GasolineraFavorita sut = new GasolineraFavorita("Gasolinera cercana a casa y de pago rapido", 1);
         db.gasolineraFavoritaDAO().insertOne(sut);
         List<GasolineraFavorita> lista = db.gasolineraFavoritaDAO().getAll();
-        Assert.assertTrue(lista.size() == 1);
-        Assert.assertTrue(lista.get(0).equals(sut));
+        Assert.assertEquals(1, lista.size());
+        Assert.assertEquals(lista.get(0), sut);
 
         // Caso 3: Tabla con mas de 1 registro
         GasolineraFavorita sut2 = new GasolineraFavorita("Gasolinera en la entrada de Santander", 5);
         db.gasolineraFavoritaDAO().insertOne(sut2);
         lista = db.gasolineraFavoritaDAO().getAll();
-        Assert.assertTrue(lista.size() == 2);
-        Assert.assertTrue(lista.get(0).equals(sut));
-        Assert.assertTrue(lista.get(1).equals(sut2));
+        Assert.assertEquals(2, lista.size());
+        Assert.assertEquals(lista.get(0), sut);
+        Assert.assertEquals(lista.get(1), sut2);
     }
 
     @Test
     public void findByIdTest(){
         // Caso 1: Tabla vacia o no encuentra el registro buscado
         List<GasolineraFavorita> lista = db.gasolineraFavoritaDAO().findById(5);
-        Assert.assertTrue(lista.size() == 0);
+        Assert.assertEquals(0, lista.size());
 
         // Caso 2: Tabla contiene el elemento buscado
         GasolineraFavorita sut = new GasolineraFavorita("Gasolinera cercana a casa y de pago rapido", 1);
         int res = db.gasolineraFavoritaDAO().getIdFromRowId(db.gasolineraFavoritaDAO().insertOne(sut));
         lista = db.gasolineraFavoritaDAO().findById(res);
-        Assert.assertTrue(lista.size() == 1);
-        Assert.assertTrue(lista.get(0).equals(sut));
+        Assert.assertEquals(1, lista.size());
+        Assert.assertEquals(lista.get(0), sut);
 
         //Caso3: Dos o mas elementos en Tabla
         GasolineraFavorita sut2 = new GasolineraFavorita("Gasolinera en la entrada de Santander", 5);
         res = db.gasolineraFavoritaDAO().getIdFromRowId(db.gasolineraFavoritaDAO().insertOne(sut2));
         lista = db.gasolineraFavoritaDAO().findById(res);
-        Assert.assertTrue(lista.size() == 1);
-        Assert.assertTrue(lista.get(0).equals(sut2));
+        Assert.assertEquals(1, lista.size());
+        Assert.assertEquals(lista.get(0), sut2);
     }
 
     @Test
@@ -78,7 +78,7 @@ public class GasolinaFavoritaDAOTest {
             ;
         } catch (Exception e)
         {
-            Assert.assertTrue(db.gasolineraFavoritaDAO().getAll().size()==0);
+            Assert.assertEquals(0, db.gasolineraFavoritaDAO().getAll().size());
         }
 
         // Caso 2: Objeto correcto
@@ -86,8 +86,8 @@ public class GasolinaFavoritaDAOTest {
         int preInsert = db.gasolineraFavoritaDAO().getAll().size();
         db.gasolineraFavoritaDAO().insertOne(sut);
         List<GasolineraFavorita> lista = db.gasolineraFavoritaDAO().getAll();
-        Assert.assertTrue((lista.size()-preInsert) == 1);
-        Assert.assertTrue(lista.get(lista.size()-1).equals(sut));
+        Assert.assertEquals(1, (lista.size() - preInsert));
+        Assert.assertEquals(lista.get(lista.size() - 1), sut);
     }
 
     @Test
@@ -99,19 +99,19 @@ public class GasolinaFavoritaDAOTest {
             Assert.fail();
         } catch (Exception e)
         {
-            Assert.assertTrue(db.gasolineraFavoritaDAO().getAll().size()==0);
+            Assert.assertEquals(0, db.gasolineraFavoritaDAO().getAll().size());
         }
 
         //Caso 2: Eliminar objeto no existente
         GasolineraFavorita sut = new GasolineraFavorita("Gasolinera cercana a casa y de pago rapido", 1);
         db.gasolineraFavoritaDAO().delete(sut);
-        Assert.assertTrue(db.gasolineraFavoritaDAO().getAll().size()==0);
+        Assert.assertEquals(0, db.gasolineraFavoritaDAO().getAll().size());
 
         //Caso 3: Eliminar objeto existente
         long rowid = db.gasolineraFavoritaDAO().insertOne(sut);
         sut.setId(db.gasolineraFavoritaDAO().getIdFromRowId(rowid));
         db.gasolineraFavoritaDAO().delete(sut);
-        Assert.assertTrue(db.gasolineraFavoritaDAO().getAll().size()==0);
+        Assert.assertEquals(0, db.gasolineraFavoritaDAO().getAll().size());
     }
 
     @Test
@@ -119,12 +119,12 @@ public class GasolinaFavoritaDAOTest {
 
         // Caso 1: Tabla vacia
         db.gasolineraFavoritaDAO().nuke();
-        Assert.assertTrue(db.gasolineraFavoritaDAO().getAll().size() == 0);
+        Assert.assertEquals(0, db.gasolineraFavoritaDAO().getAll().size());
         //Caso 2: Tabla con 1 elemento
         GasolineraFavorita sut = new GasolineraFavorita("Gasolinera cercana a casa y de pago rapido", 1);
         db.gasolineraFavoritaDAO().insertOne(sut);
         db.gasolineraFavoritaDAO().nuke();
-        Assert.assertTrue(db.gasolineraFavoritaDAO().getAll().size() == 0);
+        Assert.assertEquals(0, db.gasolineraFavoritaDAO().getAll().size());
 
         // Caso 3: Tabla con 2 o mas elementos
         GasolineraFavorita sut2 = new GasolineraFavorita("Gasolinera en la entrada de Santander", 5);
@@ -133,7 +133,7 @@ public class GasolinaFavoritaDAOTest {
         db.gasolineraFavoritaDAO().insertOne(sut2);
         db.gasolineraFavoritaDAO().insertOne(sut3);
         db.gasolineraFavoritaDAO().nuke();
-        Assert.assertTrue(db.gasolineraFavoritaDAO().getAll().size() == 0);
+        Assert.assertEquals(0, db.gasolineraFavoritaDAO().getAll().size());
     }
 
     @Test
@@ -141,12 +141,12 @@ public class GasolinaFavoritaDAOTest {
 
         // Caso 1: No existente (Puedo poner el ID que quiera debido a que la tabla se encuentra vacia)
         long res = db.gasolineraFavoritaDAO().getIdFromRowId(27);
-        Assert.assertTrue(res == 0);
+        Assert.assertEquals(0, res);
 
         // Caso 2: Correcto
         GasolineraFavorita sut = new GasolineraFavorita("Gasolinera cercana a casa y de pago rapido", 1);
         res = db.gasolineraFavoritaDAO().insertOne(sut);
-        Assert.assertTrue(res == 1);
+        Assert.assertEquals(1, res);
     }
 
     @Test
@@ -163,15 +163,15 @@ public class GasolinaFavoritaDAOTest {
         // Caso 2: Intentar actualizar una dupla que no existe en la base de datos
         GasolineraFavorita sut = new GasolineraFavorita("Gasolinera cercana a casa y de pago rapido", 1);
         db.gasolineraFavoritaDAO().update(sut);
-        Assert.assertTrue(db.gasolineraFavoritaDAO().getAll().size() == 0);
+        Assert.assertEquals(0, db.gasolineraFavoritaDAO().getAll().size());
         // Caso 3: Tabla con 1 elemento
         long rowid = db.gasolineraFavoritaDAO().insertOne(sut);
         sut.setId(db.gasolineraFavoritaDAO().getIdFromRowId(rowid));
         sut.setComentario("Pues al final no estaba tan cerca de casa pero esta bien");
         db.gasolineraFavoritaDAO().update(sut);
         List<GasolineraFavorita> lista = db.gasolineraFavoritaDAO().getAll();
-        Assert.assertTrue(lista.size() == 1);
-        Assert.assertTrue(lista.get(0).equals(sut));
+        Assert.assertEquals(1, lista.size());
+        Assert.assertEquals(lista.get(0), sut);
 
         // Caso 3: Tabla con 2 o mas elementos
         GasolineraFavorita sut2 = new GasolineraFavorita("Gasolinera en la entrada de Santander", 5);
@@ -181,7 +181,7 @@ public class GasolinaFavoritaDAOTest {
         sut3.setId(db.gasolineraFavoritaDAO().getIdFromRowId(rowid));
         sut3.setComentario("No esta a medio camino, esta casi en Castro, pero esta genial!!!");
         db.gasolineraFavoritaDAO().update(sut3);
-        Assert.assertTrue(db.gasolineraFavoritaDAO().getAll().size() == 3);
-        Assert.assertTrue(db.gasolineraFavoritaDAO().findById(db.gasolineraFavoritaDAO().getIdFromRowId(rowid)).get(0).equals(sut3));
+        Assert.assertEquals(3, db.gasolineraFavoritaDAO().getAll().size());
+        Assert.assertEquals(db.gasolineraFavoritaDAO().findById(db.gasolineraFavoritaDAO().getIdFromRowId(rowid)).get(0), sut3);
     }
 }
