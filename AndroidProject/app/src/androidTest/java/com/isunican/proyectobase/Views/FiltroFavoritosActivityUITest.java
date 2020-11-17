44fe0d2e089c36abe450c06941369d3a08d7d30f
Adrian <acf142@alumnos.unican.es>
2020-11-17 22:02:40 +0100

    Tests arreglados

--- AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/Views/FiltroFavoritosActivityUITest.java
+++ AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/Views/FiltroFavoritosActivityUITest.java
@@ -2,20 +2,26 @@ package com.isunican.proyectobase.Views;
 
 import android.widget.ListView;
 
+import androidx.test.annotation.UiThreadTest;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.base.MainThread;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import com.isunican.proyectobase.Database.AppDatabase;
 import com.isunican.proyectobase.Model.Gasolinera;
 import com.isunican.proyectobase.R;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
 
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.action.ViewActions.click;
@@ -23,6 +29,7 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.isunican.proyectobase.Presenter.PresenterGasolineras.SANTANDER;
 import static org.hamcrest.Matchers.anything;
 
 @RunWith(AndroidJUnit4.class)
@@ -37,8 +44,23 @@ public class FiltroFavoritosActivityUITest {
         ApplicationProvider.getApplicationContext().deleteDatabase("RoomDatabase");
     }
 
+    @Before
+    @UiThreadTest
+    public void setUp(){
+        ArrayList<Gasolinera> listaOriginal = new ArrayList<Gasolinera>();
+        listaOriginal.add(new Gasolinera(1000,SANTANDER,SANTANDER, "Av Valdecilla", 1.299,1.359,"AVIA"));
+        listaOriginal.add(new Gasolinera(1053,SANTANDER,SANTANDER, "Plaza Matias Montero", 0,1.349,"CAMPSA"));
+        listaOriginal.add(new Gasolinera(420,SANTANDER,SANTANDER, "Area Arrabal Puerto de Raos", 0,1.279,"E.E.S.S. MAS, S.L."));
+        listaOriginal.add(new Gasolinera(9564,SANTANDER,SANTANDER, "Av Parayas", 1.189,0,"EASYGAS"));
+        listaOriginal.add(new Gasolinera(1025,SANTANDER,SANTANDER, "Calle el Empalme", 1.259,0,"CARREFOUR"));
+        activityRule.getActivity().listaActual = listaOriginal;
+        activityRule.getActivity().adapterFavoritas.addAll(listaOriginal);
+        activityRule.getActivity().adapterFavoritas.notifyDataSetChanged();
+    }
+
     @Test
     public void filtroFavoritosMarcaTest(){
+
         //Caso 1: filtramos por una marca cualquiera
         ListView listaFav = activityRule.getActivity().findViewById(R.id.listFavGasolineras);
         //Cogemos la cantidad de favoritos antes de filtrar
