44853dea3319713ccf4b477038927502f50f61eb
Carol Corales <cca131@alumnos.unican.es>
2020-11-17 18:29:57 +0100

    WIP: Pruebas unitarias de filtrar gasolineras favoritas

--- AndroidProject/app/src/test/java/com/isunican/proyectobase/Utilities/ExtractorLocalidadUtilTest.java
+++ AndroidProject/app/src/test/java/com/isunican/proyectobase/Utilities/ExtractorLocalidadUtilTest.java
@@ -3,10 +3,13 @@ package com.isunican.proyectobase.Utilities;
 
 import com.isunican.proyectobase.Model.Gasolinera;
 
+
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+
+import static org.junit.Assert.*;
 
 /**
  * Clase de tests que contiene las pruebas unitarias de la utilidad ExtractorLocalidadUtil
@@ -28,19 +31,26 @@ public class ExtractorLocalidadUtilTest {
         filtradasLocalidad = new ArrayList<>();
         //TODO: añadir gasolineras de localidades distintas
         this.gasolineras.add(new Gasolinera(1000,"Santander","Santander", "Av Valdecilla", 1.299,1.359,"AVIA"));
-        this.gasolineras.add(new Gasolinera(1053,"Santander","Santander", "Plaza Matias Montero", 1.270,1.349,"CAMPSA"));
-        this.gasolineras.add(new Gasolinera(420,"Santander","Santander", "Area Arrabal Puerto de Raos", 1.249,1.279,"CAMPSA"));
-        this.gasolineras.add(new Gasolinera(9564,"Santander","Santander", "Av Parayas", 1.189,1.269,"EASYGAS"));
-        this.gasolineras.add(new Gasolinera(1025,"Santander","Santander", "Calle el Empalme", 1.259,1.319,"CARREFOUR"));
+        this.gasolineras.add(new Gasolinera(1053,"Santander","Santander", "Plaza Matias Montero", 1.270,1.349,"AVIA"));
+        this.filtradasLocalidad.add(gasolineras.get(0));
         this.filtradasLocalidad.add(gasolineras.get(1));
-        this.filtradasLocalidad.add(gasolineras.get(2));
     }
     @Test
     public void aplicaFiltroTest(){
-        //Caso 1
+        ArrayList<Gasolinera> listaFiltrada;
+        //Caso 1: Filtro con localidad existente y lista de gasolineras
+        listaFiltrada = (ArrayList<Gasolinera>) ExtractorLocalidadUtil.aplicaFiltro("AVIA", gasolineras);
+
+        //Caso 2: localidad no existente, Lista de gasolineras
+        assertTrue(ExtractorLocalidadUtil.aplicaFiltro("Torrelavega", gasolineras).isEmpty());
+        //Caso 3: lista vacia
+        assertTrue(ExtractorLocalidadUtil.aplicaFiltro("Santander", new ArrayList<Gasolinera>()).isEmpty());
+        //Caso 4: Lista nula
+        assertTrue(ExtractorLocalidadUtil.aplicaFiltro("Santander", null).isEmpty());
     }
    @Test
    public void extraeLocalidadesTest(){
+
 
    }
 }
